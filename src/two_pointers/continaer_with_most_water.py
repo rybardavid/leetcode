0803e879497a5b08411f82b46751c11990281731
Dávid Rýbar <david.rybar@shipmonk.com>
2024-05-11 19:55:27 +0200

    Max area more pythonic way

--- src/two_pointers/continaer_with_most_water.py
+++ src/two_pointers/continaer_with_most_water.py
@@ -3,11 +3,10 @@ def run(nums: list[int]) -> int:
     l, r, = 0, len(nums) - 1
     while l < r:
         lenght = r - l
-        height = nums[l] if nums[l] < nums[r] else nums[r]
+        height = min(nums[l], nums[r])
 
         area = lenght * height
-        if area > maxArea:
-            maxArea = area
+        maxArea = max(maxArea, area)
 
         if nums[l] < nums[r]:
             l += 1
